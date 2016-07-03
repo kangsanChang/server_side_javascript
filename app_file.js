dff7aa4c7c7a0fbd0770f75b56ca9abb605497f1
Kangsan <rkdtks1005@gmail.com>
2016-07-04 08:52:08 +0900

    중복되는 부분을 url 두개를 처리하는 router의 if문을 통해 합쳤음, url로 새글쓰기 드가지 않고 링크로 들어가게 만듦.

--- app_file.js
+++ app_file.js
@@ -10,41 +10,71 @@ app.listen(3000, function(req,res){
   console.log('Connected, 3000 port!');
 });
 app.get('/topic/new', function(req,res){
-  res.render('new');
-});
-
-app.get('/topic',function(req,res){
   fs.readdir('data', function(err, files){
-    //readdir은 file system의 경로에서 파일의 목록을 읽어주는 함수이다.
-    // callback function의 parameter 에는 err객체와 읽어온 file들의 목록이 담겨있는 files가 있다.
     if(err){
       console.log(err);
-      res.status(500).send("Internal Server Error!");
+      res.status(500).send('Internal Server error');
     }
-    res.render('view',{topics:files});
-    // render 의 두번째 인자는 그 template에 주입하고 싶은 data 적어줌
-    //읽어온 file (files) 을 topics라는 변수로 template engine에서 사용가능하게 함
-  })
-});
-
-app.get('/topic/:id', function(req,res){
-  //바뀔 수 있는 정보는 : 을 통해 적어줌
-  var id = req.params.id;
-  fs.readdir('data', function(err, files){ //특정한 parameter로 갈때도 목록은 필요
-    if(err){
-      console.log(err);
-      res.status(500).send("Internal Server Error!");
-    }
-    fs.readFile('data/'+id, 'utf8', function(err,data){
-      if(err){
-        console.log(err);
-        res.status(500).send("Internal Server Error!");
-      }
-        res.render('view', {topics: files, title: id, description: data});
-        //filename이 id값 인 애를 title이란 변수로 보냄
-    });
+  res.render('new',{topics:files});
   });
 });
+
+//중복되는 부분을 url 두개를 처리하는 router의 if문을 통해 해결
+app.get(['/topic','/topic/:id'], function(req,res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server error');
+    }
+    var id = req.params.id;
+    if(id){
+      //id 값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err,data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server error');
+        }
+        res.render('view',{topics:files, title:id, description:data});
+      })
+    } else {
+      //id값이 없을 때
+      res.render('view',{topics:files, title:"Welcome!!!!!", description:"Hello, JS for Server"})
+    }
+  })
+})
+
+// app.get('/topic',function(req,res){
+//   fs.readdir('data', function(err, files){
+//     //readdir은 file system의 경로에서 파일의 목록을 읽어주는 함수이다.
+//     // callback function의 parameter 에는 err객체와 읽어온 file들의 목록이 담겨있는 files가 있다.
+//     if(err){
+//       console.log(err);
+//       res.status(500).send("Internal Server Error!");
+//     }
+//     res.render('view',{topics:files});
+//     // render 의 두번째 인자는 그 template에 주입하고 싶은 data 적어줌
+//     //읽어온 file (files) 을 topics라는 변수로 template engine에서 사용가능하게 함
+//   })
+// });
+//
+// app.get('/topic/:id', function(req,res){
+//   //바뀔 수 있는 정보는 : 을 통해 적어줌
+//   var id = req.params.id;
+//   fs.readdir('data', function(err, files){ //특정한 parameter로 갈때도 목록은 필요
+//     if(err){
+//       console.log(err);
+//       res.status(500).send("Internal Server Error!");
+//     }
+//     fs.readFile('data/'+id, 'utf8', function(err,data){
+//       if(err){
+//         console.log(err);
+//         res.status(500).send("Internal Server Error!");
+//       }
+//         res.render('view', {topics: files, title: id, description: data});
+//         //filename이 id값 인 애를 title이란 변수로 보냄
+//     });
+//   });
+// });
 
 app.post('/topic', function(req, res){
   var title = req.body.title;
@@ -54,6 +84,6 @@ app.post('/topic', function(req, res){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.send('Success!');
+    res.redirect('/topic/'+title); //redirect해서 form 제출 후 보내버리고 싶은 경로로 이동할 수 있게함
   });
 });
